1ae61362581905680a4c729e504e139f0f16f3b9
Athish Venkatesh <athishaves@gmail.com>
2023-11-27 22:51:34 +0530

    pr comments ; added unit tests

--- lib/src/num_ext.dart
+++ lib/src/num_ext.dart
@@ -1,32 +1,24 @@
 /// Int extensions
 extension IntExt on int {
-  /// Returns ordinal of [int]
+  /// Converts [int] into English ordinal representation
   /// ``` dart
   /// print(1.ordinal); // 1st
   /// print(22.ordinal); // 22nd
   /// print(143.ordinal); // 143rd
   /// print(0.ordinal); // 0th
-  /// print((-1).ordinal); // -1st
   /// print(12.ordinal); // 12th
   /// print(69.ordinal); // 69th
   /// ```
   String get ordinal {
-    if (this < 0) {
-      return '-${(-this).ordinal}';
-    }
-    if (this % 100 >= 11 && this % 100 <= 13) {
-      return '${this}th';
-    }
-    switch (this % 10) {
-      case 1:
-        return '${this}st';
-      case 2:
-        return '${this}nd';
-      case 3:
-        return '${this}rd';
-      default:
-        return '${this}th';
-    }
+    return switch (this % 100) {
+      11 || 12 || 13 => '${this}th',
+      _ => switch (this % 10) {
+          1 => '${this}st',
+          2 => '${this}nd',
+          3 => '${this}rd',
+          _ => '${this}th'
+        }
+    };
   }
 
   /// Returns roman number representation of [int] from 1 to 3999
@@ -36,52 +28,37 @@ extension IntExt on int {
   /// print(1.toRoman); // I
   /// print(3999.toRoman); // MMMCMXCIX
   /// ```
-  String get toRoman {
+  String get roman {
     if (this < 1 || this > 3999) {
       throw Exception('Number out of range (1 to 3999)');
     }
 
-    final romanNumerals = [
-      'I',
-      'IV',
-      'V',
-      'IX',
-      'X',
-      'XL',
-      'L',
-      'XC',
-      'C',
-      'CD',
-      'D',
-      'CM',
-      'M',
-    ];
-    final decimalValues = [
-      1,
-      4,
-      5,
-      9,
-      10,
-      40,
-      50,
-      90,
-      100,
-      400,
-      500,
-      900,
-      1000,
-    ];
+    const romanTable = {
+      'M': 1000,
+      'CM': 900,
+      'D': 500,
+      'CD': 400,
+      'C': 100,
+      'XC': 90,
+      'L': 50,
+      'XL': 40,
+      'X': 10,
+      'IX': 9,
+      'V': 5,
+      'IV': 4,
+      'I': 1,
+    };
 
-    final resultBuffer = StringBuffer();
-    var index = 12;
-    var temp = this;
-
-    while (temp > 0) {
-      final div = temp ~/ decimalValues[index];
-      temp %= decimalValues[index];
-      resultBuffer.write(romanNumerals[index--] * div);
+    final result = StringBuffer();
+    var n = this;
+    for (final entry in romanTable.entries) {
+      final numeral = entry.key;
+      final value = entry.value;
+      while (n >= value) {
+        result.write(numeral);
+        n -= value;
+      }
     }
-
-    return resultBuffer.toString();
+    return result.toString();
   }
 }
